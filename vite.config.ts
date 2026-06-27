26822e1004344645eb0b736b865204f70fdc33ce
Kashi <kashi.sabarish@gmail.com>
2026-06-27 11:01:10 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/IAN-Visuals/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
